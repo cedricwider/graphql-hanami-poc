c644906590d2c0d1c2bd9f2d5ac11a72ff612289
Cedric Wider <wider.cedric@gmail.com>
2016-09-24 19:28:08 +0300

    add api application

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/graphql'
+require_relative '../apps/api/application'
 require_relative '../apps/web/application'
 
 Hanami::Container.configure do
+  mount Api::Application, at: '/api'
   mount Web::Application, at: '/'
 end
