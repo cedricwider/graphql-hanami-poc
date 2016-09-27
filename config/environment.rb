7e33ff30401d6d6027b0629453d9d204cea1a78d
Cedric Wider <wider.cedric@gmail.com>
2016-09-27 22:05:14 +0200

    get rid of apps/web

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,7 @@ require 'bundler/setup'
 require 'hanami/setup'
 require_relative '../lib/graphql'
 require_relative '../apps/api/application'
-require_relative '../apps/web/application'
 
 Hanami::Container.configure do
   mount Api::Application, at: '/api'
-  mount Web::Application, at: '/'
 end
